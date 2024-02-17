1509dcd452039bfe088f3133eaa421d91d02bcbf
amr11511 <137119230+amr11511@users.noreply.github.com>
2024-02-17 20:57:24 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactComponent,
+    NotFoundPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.css']
+})
+export class PortfolioComponent {
+  imgesSrc:string[]= ['./assets/imgs/poert1.png','./assets/imgs/port2.png','./assets/imgs/port3.png'];
+  img1Flag:boolean = false;
+  img2Flag:boolean = false;
+  img3Flag:boolean = false;
+  showModel1(){
+    this.img1Flag = true;
+  }
+  showModel2(){
+    this.img2Flag = true;
+  }
+  showModel3(){
+    this.img3Flag = true;
+  }
+  closeModel(){
+    this.img1Flag=false;
+    this.img2Flag=false;
+    this.img3Flag=false;
+  };
+
+  backimg1(){
+    this.img1Flag = false;
+    this.img3Flag = true;
+  }
+  forwardimg1(){
+    this.img1Flag = false;
+    this.img2Flag = true;
+  }
+  backimg2(){
+    this.img2Flag = false;
+    this.img1Flag = true;
+  }
+  forwardimg2(){
+    this.img2Flag = false;
+    this.img3Flag = true;
+  }
+  backimg3(){
+    this.img3Flag = false;
+    this.img2Flag = true;
+  }
+  forwardimg3(){
+    this.img3Flag = false;
+    this.img1Flag = true;
+  }
+}
